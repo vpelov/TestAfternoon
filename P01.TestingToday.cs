47f2abb2e70878f5de8a59f956b586877eadf0dc
VlPelov <vpelov@mail.bg>
2022-02-21 13:54:21 +0200

    Add files via upload

---
+++ P01.TestingToday.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P01.TestingToday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Last Testing!");
+            Console.WriteLine("Last Testing!");
+
+
+        }
+    }
+}
